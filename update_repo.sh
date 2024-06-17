9e06f0fb493166684f4a338bec91a8295aa442b1
andres <lianone@gmail.com>
2024-06-16 23:57:12 -0300

    subida prueba6Commit

--- update_repo.sh
+++ update_repo.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 #cd /home/andres/Documents/cursos/ultimate_python
-git pull origin main
-git add .
-git commit -m "$"
-git push origin main
+#git pull origin main
+#git add .
+#git commit -m "$"
+#git push origin main
 
+
+
+# Solicitar el mensaje del commit al usuario
+echo "Introduce el mensaje del commit:"
+read commit_message
+
+# Actualizar el repositorio
+git pull origin main
+
+# Añadir todos los cambios
+git add .
+
+# Hacer commit con el mensaje proporcionado por el usuario
+git commit -m "$commit_message"
+
+# Enviar los cambios al repositorio remoto
+git push origin main
