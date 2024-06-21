ae2c2d6f0b1b7b9ff576652f70e74c5fdfb9cbed
andres <lianone@gmail.com>
2024-06-21 19:24:00 -0300

    clase 41

---
+++ curso_py/funciones/05-alcance.py
@@ -0,0 +1,17 @@
+saludo = "Hola global"  # no utilizar variables globales nunca
+
+
+def saludar():
+    saludo = "Hola Mundo"
+    print(saludo)
+
+
+def saludaPerrito():
+    saludo = "Hola Perrita"
+    print(saludo)
+
+
+saludar()
+saludaPerrito()
+saludar()
+print(saludo)
